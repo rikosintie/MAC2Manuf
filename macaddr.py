267639a6081649c07afefadef384364da8b67ba4
Michael Hubbard <michael.hubbard999@gmail.com>
2018-02-22 05:13:01 +0000

    Cleaned up some lines of code
    Added error checking during file load, changed the way the lines are selected, cleaned up the MAC selection code.

--- macaddr.py
+++ macaddr.py
@@ -12,7 +12,6 @@ show mac add int g1/0/4 | i Gi
 
 and create a list of Vlan, Mac Address, interface and manufacturer.
 
-
 test-switch#show mac add int g1/0/1 | i Gi
   10    8434.97a7.708b    DYNAMIC     Gi1/0/1
 test-switch#show mac add int g1/0/2 | i Gi
@@ -20,14 +19,14 @@ test-switch#show mac add int g1/0/3 | i Gi
   10    0c4d.e9c1.4a0d    DYNAMIC     Gi1/0/3
 test-switch#show mac add int g1/0/4 | i Gi
 
-
 Output
-# Entries: 65 
+Number Entries: 65 
+
 Vlan     MAC Address      Interface
-  10    8434.97a7.708b     Gi1/0/1
-**************************************
-  10    0c4d.e9c1.4a0d     Gi1/0/3
-**************************************
+  10    8434.97a7.708b     Gi1/0/1   Vendor(manuf='HewlettP', comment=None)
+****************************************************************************
+  10    0c4d.e9c1.4a0d     Gi1/0/3   Vendor(manuf='Apple', comment=None)
+****************************************************************************
 
 Uses the Parser library for Wireshark's OUI database from https://github.com/coolbho3k/manuf to convert the MAC to a manufacture.
 The database needs to be updated occaisionally using: 
@@ -44,30 +43,35 @@ import manuf
 print()
 p = manuf.MacParser()
 #create a blank list to accept each line in the file
-listname = []
-f = open('mac-addr.txt', 'r')
-for line in f:
-    listname.append(line)
-f.close
-# string length
-i = len(listname)-1
-#d = i + 1
+data = []
+try:
+    f = open('mac-addr.txt', 'r')
+except FileNotFoundError:
+            print('mac-addr.txt does not exist')
+else:    
+#f = open('vlans.txt', 'r')
+    for line in f:
+#strip out empty lines
+        if line.strip():
+            data.append(line)
+    f.close
+ct = len(data)-1
 counter = 0
 IPs = []
-data = {}
-while counter <= i:
-    IP = listname[counter]
+while counter <= ct:
+    IP = data[counter]
 #Remove Enter
     IP = IP.strip('\n')
 #skip any line with the command
-    if IP.find('show') != -1:
+    #if IP.find('show') != -1:
+    if IP.find('DYNAMIC') == -1:
         counter = counter + 1
         continue
 #extract MAC Address
 #   2    6cb2.ae09.f8c4    DYNAMIC     Gi2/0/2
-    Mac = IP
-    Dot = Mac.find('.')
-    Mac = Mac[Dot-4:Dot+10].rstrip('')
+    L = str.split(IP)
+    Mac = L[1]
+#    print(Mac)
 #pull the manufacturer with manuf
     manufacture = p.get_all(Mac)
 #strip out the word DYNAMIC
